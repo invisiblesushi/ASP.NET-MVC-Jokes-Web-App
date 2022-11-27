3587c36050cff36270e5eb325942fd2e32b8752b
Daniel <danielcoco96@gmail.com>
2022-11-27 18:29:46 +0100

    Added Jokes controller

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        [Key]
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set;}
+
+        public Joke()
+        {
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ASP.NET-MVC-Jokes-App
  
+- Model = classes (objects)
+  - Manages the behavior and data
+- View = web page (Razor HTML)
+  - Manages the display of data
+- Controller = connects models, business logic and web pages
+  - Handles page evens and navigation
